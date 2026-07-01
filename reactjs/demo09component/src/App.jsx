9137d635468c27eb568f4e76d1ba9e6bbb9f47f1
YSH0209 <yoonseunghoon0209@gmail.com>
2026-07-01 16:07:08 +0900

    Exam01 분할

--- reactjs/demo09component/src/App.jsx
+++ reactjs/demo09component/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Jumbotron from "./components/Jumbotron" //jsx생략
+import Exam01 from "./components/Exam01"
+
 
 function App() {
 
+
   return (
    <div className="container my-5">
-    <Jumbotron title="객체 state 다루기" content="입력창 여러개를 하나의 state로 관리하는 법을 배웁니다"/>
-    
+    <Exam01/>
+
+    <hr/>
+   
+
    </div>
   )
 }
