d40564b821899bbbd1816c002c2df4eb22f0b808
YSH0209 <yoonseunghoon0209@gmail.com>
2026-07-10 17:20:56 +0900

    성능저하 해결

--- reactjs/home/src/components/country/CountrySearch.jsx
+++ reactjs/home/src/components/country/CountrySearch.jsx
@@ -2,6 +2,7 @@ import Jumbotron from "@templates/Jumbotron";
 import { useCallback, useEffect, useState } from "react";
 import { Col, Form, ListGroup, Row } from "react-bootstrap";
 import axios from "axios";
+import { throttle, debounce } from "lodash-es";
 
 
 export default function CountrySearch() {
@@ -19,25 +20,29 @@ export default function CountrySearch() {
     //effect
     // - keyword가 변하면 ajax 요청을 서버로 전송
     useEffect(()=>{
-        if(keyword.length === 0) {
-            setSearchList([]);
-            return;
-        }
-        
-        searchKeyword();
+        searchKeyword(keyword);
     }, [keyword]);
 
-    const searchKeyword = useCallback(async()=>{
-        console.log(composition, keyword);
+    //throttle, debounce 설정 시 주의사항
+    //throttle(함수, 실행주기)를 설정하면 -> 새로운 함수가 생성됨
+    //debounce(함수, 실행주기)를 설정하면 -> 새로운 함수가 생성됨
+    //- (주의) 함수를 만들 때 연관항목을 설정하지 말아야 한다 (함수가 재생성이 안되야 함)
+    //- 일반적으로 실행주기는 250ms ~ 350ms 정도가 적당 (1초에 3~4번)
+
+    const searchKeyword = useCallback (throttle (async(keyword)=>{
         if(keyword.length === 0) {
             setSearchList([]);
             return;
         }
 
-
+        console.log("searchKeyword 실행");
+        
         const response = await axios.get(`/api/country/countryName/${keyword}`);
         setSearchList(response.data);
-    },[keyword, composition]);
+    }, 250),[]);
+
+
+
 
     return (<>
         <Jumbotron title="국가명 검색 샘플" />
