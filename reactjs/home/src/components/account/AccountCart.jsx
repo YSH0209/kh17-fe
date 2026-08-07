af61145a69d08e3f1e6dc6065d2ae9c39c70000d
YSH0209 <yoonseunghoon0209@gmail.com>
2026-08-07 17:01:42 +0900

    가격 나타내기

--- reactjs/home/src/components/account/AccountCart.jsx
+++ reactjs/home/src/components/account/AccountCart.jsx
@@ -1,74 +1,74 @@
 import Jumbotron from "@templates/Jumbotron";
 import { apiClient } from "@utils/reaxios";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { Col, Form, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { Button, Col, Form, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import NoImage from "@assets/images/no-image.png";
-import { FaArrowTrendDown } from "react-icons/fa6";
+import { FaArrowTrendDown, FaCartShopping } from "react-icons/fa6";
 
 export default function AccountCart() {
 
     //시작하자마자 요청을 보내 받아온 장바구니 목록을 표시
     const [cartList, setCartList] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         loadData();
     }, []);
 
-    const loadData = useCallback(async ()=>{
+    const loadData = useCallback(async () => {
         const { data } = await apiClient.get("/cart/");
         // setCartList(data.cartItems); //체크 미설정
         setCartList(data.cartItems.map(
-            item=>({...item, choice : true})
+            item => ({ ...item, choice: true })
         ));
         //console.log(data.cartItems);
     }, []);
 
     //할인율 계산 함수
-    const calculateDiscountRate = useCallback((item)=>{
-        if(item.origin <= item.discount) return 0;
-        if(item.discount === 0) return 100;
+    const calculateDiscountRate = useCallback((item) => {
+        if (item.origin <= item.discount) return 0;
+        if (item.discount === 0) return 100;
         const discount = item.origin - item.discount;
         const rate = discount * 100 / item.origin;
         return rate.toFixed(0);//소수점 2자리
     }, []);
 
     // 수량 변경 함수 (수량이 변경되면 서버에 바로 반영할것인지 결정)
-    const changeItemQty = useCallback((e, target)=>{
+    const changeItemQty = useCallback((e, target) => {
         const { value } = e.target;
         const replacement = value.replace(/[^0-9]+/g, "");
         const number = parseInt(replacement) || 1;
 
         setCartList(
-            prev=>prev.map(
+            prev => prev.map(
                 item => {
-                    if(item.no === target.no) {//찾는 상품이면
+                    if (item.no === target.no) {//찾는 상품이면
                         //number가 qty에 적용된 객체 반환
-                        return { ...item , qty : number };
+                        return { ...item, qty: number };
                     }
-                    return { ... item };//나머지는 그대로 반환
+                    return { ...item };//나머지는 그대로 반환
                 }
             )
         );
     }, []);
 
     //항목 체크
-    const changeItemSelected = useCallback((e, target)=>{
+    const changeItemSelected = useCallback((e, target) => {
         const { checked } = e.target;
 
         setCartList(
-            prev=>prev.map(
+            prev => prev.map(
                 item => {
-                    if(item.no === target.no) {//찾는 상품이면
-                        return { ...item , choice : checked };
+                    if (item.no === target.no) {//찾는 상품이면
+                        return { ...item, choice: checked };
                     }
-                    return { ... item };//나머지는 그대로 반환
+                    return { ...item };//나머지는 그대로 반환
                 }
             )
         );
     }, []);
 
     //전체 선택 관련
-    const checkedAll = useMemo(()=>{
+    const checkedAll = useMemo(() => {
         // let all = true;
         // for(let i=0; i < cartList.length; i++) {
         //     all = all && cartList[i].choice;
@@ -76,29 +76,41 @@ export default function AccountCart() {
         // return all;
 
         //return cartList.reduce(계산함수, 초기값);
-        return cartList.reduce((acc, cur) => acc && cur.choice ===true , true );
+        return cartList.reduce((acc, cur) => acc && cur.choice === true, true);
     }, [cartList]);
 
-    const toggleAll = useCallback(e=>{
+    const toggleAll = useCallback(e => {
         const { checked } = e.target;
-        setCartList(prev=>prev.map(
-            item=>({ ...item, choice : checked })
+        setCartList(prev => prev.map(
+            item => ({ ...item, choice: checked })
         ))
     }, []);
 
     //[1] 체크된 상품의 총 계산금액을 구하여 하단에 출력
-    const totalAmount = useMemo(()=>{
-       return cartList.reduce((acc,cur)=>{
-            if(cur.choice === true){ //체크되어있다면
+    const totalAmount = useMemo(() => {
+        return cartList.reduce((acc, cur) => {
+            if (cur.choice === true) { //체크되어있다면
                 return acc + cur.discount * cur.qty; //할인가를 합산
             }
             return acc; //아니면 그대로 반환
-       },0);
+        }, 0);
     }, [cartList]);
 
     //[2] 체크된 상품의 할인전/후 금액을 각각 구하여 하단에 출력 (=gmarket)
+    const totalAmountObject = useMemo(() => {
+        return cartList.reduce(
+            (acc, cur) => { //acc가 객체 (origin, discount 라는 필드가 존재)
+                if (cur.choice === true) { //체크되어 있는 경우엔
+                    return {
+                        origin: acc.origin + cur.origin,
+                        discount: acc.discount + cur.discount
+                    }
+                }
+                return acc;
+            },
+            { origin: 0, discount: 0 }) //초기값이 객체
+    }, [cartList])
 
-    
 
     return (<>
         <Jumbotron title="장바구니" content="상품 수량을 확인하고 구매를 진행해주세요" />
@@ -107,69 +119,106 @@ export default function AccountCart() {
         <Row className="mt-5">
             <Col>
                 <div className="mb-2">
-                    <Form.Check type="checkbox" label="전체 선택" 
+                    <Form.Check type="checkbox" label="전체 선택"
                         checked={checkedAll}
-                        onChange={toggleAll}/>
+                        onChange={toggleAll} />
                 </div>
                 <ListGroup>
-                    {cartList.map(item=>(
-                    <ListGroupItem key={item.no}>
-                        <div className="d-flex align-items-center">
-                            <Form.Check type="checkbox" className="me-2"
-                                checked={item.choice === true}
-                                onChange={e=>changeItemSelected(e, item)}/>
+                    {cartList.map(item => (
+                        <ListGroupItem key={item.no}>
+                            <div className="d-flex align-items-center">
+                                <Form.Check type="checkbox" className="me-2"
+                                    checked={item.choice === true}
+                                    onChange={e => changeItemSelected(e, item)} />
 
-                            <img src={
-                                item.thumbnail ?
-                                `${import.meta.env.VITE_SERVER_URL}/api/attach/${item.thumbnail}`
-                                    : NoImage
-                            } width={100}/>
+                                <img src={
+                                    item.thumbnail ?
+                                        `${import.meta.env.VITE_SERVER_URL}/api/attach/${item.thumbnail}`
+                                        : NoImage
+                                } width={100} />
 
-                            <div className="ms-4 flex-grow-1">
-                                <h4 className="fw-bold text-info">
-                                    {item.name}
-                                </h4>
-                                <div className="text-start">
-                                    {item.origin > item.discount ? (<>
-                                    <s className="text-muted">
-                                        {item.origin.toLocaleString()} 원
-                                    </s>
-                                    <br/>
-                                    <b className="text-danger">
-                                        {item.discount.toLocaleString()} 원
-                                    </b>
-                                    <br/>
-                                    <span className="text-success">
-                                        <FaArrowTrendDown style={{transform:"rotate(55deg)"}}/> 
-                                        {calculateDiscountRate(item)}%
-                                    </span>
-                                    </>) : (<>
-                                    <b>{item.origin.toLocaleString()} 원</b>
-                                    </>) }
-                                </div>
-                                <div className="text-end">
-                                    <span>수량 : </span>
-                                    <Form.Control type="number" inputMode="numeric"
-                                        value={item.qty} min={1} 
-                                        onChange={e=>changeItemQty(e, item)}
-                                        className="d-inline-block mx-2"
-                                        style={{width:75}}
+                                <div className="ms-4 flex-grow-1">
+                                    <h4 className="fw-bold text-info">
+                                        {item.name}
+                                    </h4>
+                                    <div className="text-start">
+                                        {item.origin > item.discount ? (<>
+                                            <s className="text-muted">
+                                                {item.origin.toLocaleString()} 원
+                                            </s>
+                                            <br />
+                                            <b className="text-danger">
+                                                {item.discount.toLocaleString()} 원
+                                            </b>
+                                            <br />
+                                            <span className="text-success">
+                                                <FaArrowTrendDown style={{ transform: "rotate(55deg)" }} />
+                                                {calculateDiscountRate(item)}%
+                                            </span>
+                                        </>) : (<>
+                                            <b>{item.origin.toLocaleString()} 원</b>
+                                        </>)}
+                                    </div>
+                                    <div className="text-end">
+                                        <span>수량 : </span>
+                                        <Form.Control type="number" inputMode="numeric"
+                                            value={item.qty} min={1}
+                                            onChange={e => changeItemQty(e, item)}
+                                            className="d-inline-block mx-2"
+                                            style={{ width: 75 }}
                                         />
-                                    <span>개</span>
+                                        <span>개</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </ListGroupItem>
+                        </ListGroupItem>
                     ))}
                 </ListGroup>
             </Col>
         </Row>
-
-        <Row className= "mt-5">
+        {/* 
+        <Row className= "mt-5 text-end fw-bold text-info fs-2">
             <Col className="text-end fs-2 fw-bold text-info">
                 총 {totalAmount.toLocaleString()}원
             </Col>
+        </Row> */}
+
+        <Row className="mt-5">
+            <Col>
+                <div className="d-flex justify-content-between">
+                    <span>
+                        판매금액
+                    </span>
+                    <span>
+                        {totalAmountObject.origin.toLocaleString()}원
+                    </span>
+                </div>
+                <div className="d-flex justify-content-between">
+                    <span>
+                        할인금액
+                    </span>
+                    <span>
+                        {(totalAmountObject.origin - totalAmountObject.discount).toLocaleString}원
+                    </span>
+                </div>
+                <div className="d-flex justify-content-between">
+                    <span>
+                        결제금액
+                    </span>
+                    <span>
+                        {totalAmountObject.discount.toLocaleString()}원
+                    </span>
+                </div>
+            </Col>
         </Row>
 
+        <Row className="mt-5">
+            <Col>
+                <Button variant="success" size="lg" className="w-100">
+                    <FaCartShopping/>
+                    <span className="ms-2">구매하기</span>
+                </Button>
+            </Col>
+        </Row>
     </>)
 }
