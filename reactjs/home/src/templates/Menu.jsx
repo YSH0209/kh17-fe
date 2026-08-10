25f7a51baaa0690f2ac99be77f6b0c9d163745b8
YSH0209 <yoonseunghoon0209@gmail.com>
2026-08-10 12:32:19 +0900

    Ws 클라이언트 연결코드 구성

--- reactjs/home/src/templates/Menu.jsx
+++ reactjs/home/src/templates/Menu.jsx
@@ -84,11 +84,14 @@ export default function Menu() {
                             <NavDropdown.Item as={Link} to="/book/list">도서정보</NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/book/spa">도서정보(SPA)</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title="카카오페이" id="basic-nav-dropdown">
+                        <NavDropdown title="결제" id="basic-nav-dropdown">
                             <NavDropdown.Item as={Link} to="/pay/v1/buy">무식한 결제</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/sale/list">상품목록</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="웹소켓" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/websocket/v1">기본웹소켓</NavDropdown.Item>
                         </NavDropdown>
                         {/* <Nav.Link as={Link} to="/session/test">세션테스트</Nav.Link> */}
-                        <Nav.Link as={Link} to="/sale/list">상품목록</Nav.Link>
                     </Nav>
                     <Nav>
                         { isLogin === true && (<>
