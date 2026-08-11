6554a8fce0a43e6c1f50bcee220875041ef9931e
YSH0209 <yoonseunghoon0209@gmail.com>
2026-08-11 18:21:21 +0900

    오류 수정 후 업로드

--- reactjs/home/src/templates/Body.jsx
+++ reactjs/home/src/templates/Body.jsx
@@ -58,6 +58,7 @@ import KakaopayBuyDetailVersion2 from "@components/pay/v2/KakaopayBuyDetailVersi
 
 import WebSocketV1BasicClient from "@components/websocket/WebSocketV1BasicClient";
 import WebSocketV2AdvancedClient from "@components/websocket/WebSocketV2AdvancedClient";
+import WebSocketV3MemberClient from "@components/websocket/WebSocketV3MemberClient";
 
 export default function Body() {
 
@@ -121,6 +122,7 @@ export default function Body() {
         {/* 웹소켓 */}
         <Route path="/websocket/v1" element={<WebSocketV1BasicClient/>}/>
         <Route path="/websocket/v2" element={<WebSocketV2AdvancedClient/>}/>
+         <Route path="/websocket/v3" element={<Private><WebSocketV3MemberClient/></Private>}/>
 
         {/* error */}
         <Route path="/account/block" element={<AccountBlock/>}/>
